8e86447e662cb0748b97750de0f55bb3ea14339a
master <rudolifrudolif@gmail.com>
2021-04-04 22:20:32 +0300

    edits oferta wp

--- apres/template/oferta.php
+++ apres/template/oferta.php
@@ -6,102 +6,129 @@
 	get_header();
 ?>
 
-	<main class="main">
+	<section class="about oferta">
+		<div class="container">
 
-		<section class="oferta tabs">
-			<div class="container">
-
-				<h2 class="oferta__title h2">Oferta</h2>
-
-				<div class="oferta__list tabs__list">
-					<div class="oferta__item oferta__item--one oferta__item--active tabs__item">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/about__list_img1.png" alt="" class="oferta__item_img">
-						<div class="oferta__item_content">
-							<h3>Panele fotowoltaiczne</h3>
-							<p>
-								Panele wykorzystywane w
-								naszych instalacjach produkowane są
-								przez najlepszych i najbardziej
-								uznanych producentów na świecie.
-							</p>
-						</div>
-					</div>
-					<div class="oferta__item oferta__item--two tabs__item">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/about__list_img2.png" alt="" class="oferta__item_img">
-						<div class="oferta__item_content">
-							<h3>Falowniki</h3>
-							<p>
-								Falowniki w instalacjach
-								fotowoltaicznych powinny
-								charakteryzować się wysoką
-								sprawnością i niezawodnością działania.
-								Poznaj naszą ofertę falowników!
-							</p>
-						</div>
-					</div>
-					<div class="oferta__item oferta__item--three tabs__item">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/about__list_img3.png" alt="" class="oferta__item_img">
-						<div class="oferta__item_content">
-							<h3> Systemy montażowe oraz zabezpieczenia elektryczne</h3>
-							<p>
-								Podstawą dobrze
-								wykonanej instalacją są
-								odpowiednie systemy montażowe
-								oraz zabezpieczenia elektryczne.
-								Korzystamy ze sprawdzonych
-								polskich producentów!
-							</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="oferta__logo">
-					<div class="oferta__wrap tabs__wrap">
-						<div class="oferta__logo_list">
-							<a href="#" class="oferta__logo_item modal--risen">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/risenenergy.png" alt="">
-							</a>
-							<a href="#" class="oferta__logo_item modal--lg">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/LG.png" alt="">
-							</a>
-							<a href="#" class="oferta__logo_item modal--qcells">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/Qcells.png" alt="">
-							</a>
-						</div>
-					</div>
-					<div class="oferta__wrap tabs__wrap">
-						<div class="oferta__logo_list">
-							<a href="#" class="oferta__logo_item modal--fronius">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/Fronius.png" alt="">
-							</a>
-							<a href="#" class="oferta__logo_item modal--solaredge">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/solaredge.png" alt="">
-							</a>
-							<a href="#" class="oferta__logo_item modal--SolarEdgeINV">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/Sofarsolar.png" alt="">
-							</a>
-						</div>
-					</div>
-					<div class="oferta__wrap tabs__wrap">
-						<div class="oferta__logo_list">
-							<a href="#" class="oferta__logo_item modal--corab">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/Corab.png" alt="">
-							</a>
-							<a href="#" class="oferta__logo_item modal--DEHN">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta/Dehn.png" alt="">
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="oferta__btn">
-					<a href="#" class="btn modal--two">Skontaktuj się z nami!</a>
-				</div>
-
+			<div class="about__bunner oferta__bunner">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta.jpg" alt="" class="oferta__bunner_img">
+				<h2 class="about__title h2">Oferta</h2>
 			</div>
-		</section>
 
-	</main>
+			<div class="oferta__list click">
+				<a href="#panel" class="oferta__list_item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta__list_item1.jpg" alt="" class="oferta__list_img">
+					<h3 class="oferta__list_title">PANELE FOTOWOLTAICZNE</h3>
+					<p class="oferta__list_text">
+						Panele wykorzystywane w naszych instalacjach produkowane są przez najlepszych i najbardziej 
+						uznanych producentów na świecie.
+					</p>
+				</a>
+
+				<a href="#inverters" class="oferta__list_item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta__list_item2.jpg" alt="" class="oferta__list_img">
+					<h3 class="oferta__list_title">FALOWNIKI</h3>
+					<p class="oferta__list_text">
+						Falowniki w instalacjach fotowoltaicznych powinny charakteryzować się wysoką sprawnością i 
+						niezawodnością działania. Poznaj naszą ofertę falowników!
+					</p>
+				</a>
+
+				<a href="#system" class="oferta__list_item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta__list_item3.jpg" alt="" class="oferta__list_img">
+					<h3 class="oferta__list_title">SYSTEMY <br>MONTAŻOWE ORAZ ZABEZPIECZENIA ELEKTRYCZNE</h3>
+					<p class="oferta__list_text">
+						Podstawą dobrze wykonanej instalacją są odpowiednie systemy montażowe oraz zabezpieczenia 
+						elektryczne. Korzystamy ze sprawdzonych polskich producentów!
+					</p>
+				</a>
+			</div>
+
+			<?php if( have_rows('panel') ): ?>
+				<div class="about__bunner oferta__bunner" id="panel">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta2.jpg" alt="" class="oferta__bunner_img">
+					<h2 class="about__title h2">PANELE FOTOWOLTAICZNE</h2>
+				</div>
+
+				<div class="oferta__wrap">
+					<?php while( have_rows('panel') ): the_row();
+						$logo = get_sub_field('logo');
+						$img = get_sub_field('img');
+						$desc = get_sub_field('desc');
+					?>
+
+					<div class="oferta__wrap_item">
+						<img src="<?php echo $logo; ?>" class="oferta__wrap_logo">
+						<div class="oferta__wrap_img">
+							<img src="<?php echo $img; ?>" class="oferta__wrap_logo">
+						</div>
+						<div class="oferta__wrap_desc">
+							<?php echo $desc; ?>
+						</div>
+					</div>
+
+					<?php endwhile; ?>
+					</div>
+			<?php endif; ?>
+
+
+			<?php if( have_rows('inverters') ): ?>
+				<div class="about__bunner oferta__bunner" id="inverters">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta3.jpg" alt="" class="oferta__bunner_img">
+					<h2 class="about__title h2">Falowniki</h2>
+				</div>
+
+				<div class="oferta__wrap">
+					<?php while( have_rows('inverters') ): the_row();
+						$logo = get_sub_field('logo');
+						$img = get_sub_field('img');
+						$desc = get_sub_field('desc');
+					?>
+
+					<div class="oferta__wrap_item">
+						<img src="<?php echo $logo; ?>" class="oferta__wrap_logo">
+						<div class="oferta__wrap_img">
+							<img src="<?php echo $img; ?>" class="oferta__wrap_logo">
+						</div>
+						<div class="oferta__wrap_desc">
+							<?php echo $desc; ?>
+						</div>
+					</div>
+
+					<?php endwhile; ?>
+					</div>
+			<?php endif; ?>
+
+
+			<?php if( have_rows('system') ): ?>
+				<div class="about__bunner oferta__bunner" id="system">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/oferta4.jpg" alt="" class="oferta__bunner_img">
+					<h2 class="about__title h2">SYSTEMY MONTAŻOWE ORAZ ZABEZPIECZENIA ELEKTRYCZNE</h2>
+				</div>
+
+				<div class="oferta__wrap">
+					<?php while( have_rows('system') ): the_row();
+						$logo = get_sub_field('logo');
+						$img = get_sub_field('img');
+						$desc = get_sub_field('desc');
+					?>
+
+					<div class="oferta__wrap_item">
+						<img src="<?php echo $logo; ?>" class="oferta__wrap_logo">
+						<div class="oferta__wrap_img">
+							<img src="<?php echo $img; ?>" class="oferta__wrap_logo">
+						</div>
+						<div class="oferta__wrap_desc">
+							<?php echo $desc; ?>
+						</div>
+					</div>
+
+					<?php endwhile; ?>
+					</div>
+			<?php endif; ?>
+
+		</div>
+	</section>
+
 
 	<?php get_template_part( 'parts/oferta' ); ?>
 
